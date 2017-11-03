f99fa871396dc3e5831c6443e450be0aa4feb9b7
Cesar Noreña <cnorena@tappsi.co>
2017-11-03 18:03:44 -0500

    Update get pokedex use case

--- app/src/main/kotlin/co/cesarnorena/pokedex/domain/interactors/GetPokedex.kt
+++ app/src/main/kotlin/co/cesarnorena/pokedex/domain/interactors/GetPokedex.kt
@@ -1,16 +1,23 @@
 package co.cesarnorena.pokedex.domain.interactors
 
 import co.cesarnorena.pokedex.data.model.Pokedex
+import co.cesarnorena.pokedex.domain.repository.LocalRepository
 import co.cesarnorena.pokedex.domain.repository.RemoteRepository
 import io.reactivex.Single
 
-class GetPokedex(private val remoteRepository: RemoteRepository)
+class GetPokedex(private val remoteRepository: RemoteRepository,
+                 private val localRepository: LocalRepository)
     : SingleUseCase<GetPokedex.Input, GetPokedex.Output>() {
 
     override fun executeUseCase(values: Input?): Single<Output> {
         if (values == null) return Single.error { IllegalStateException() }
+
         return remoteRepository.getPokedex(values.id)
-                .map { Output(it) }
+                .doOnSuccess {
+                    localRepository.savePokedex(it.pokedexEntries)
+                            .subscribe()
+                }
+                .map(::Output)
     }
 
     data class Input(val id: Int) : SingleUseCase.Input
