36a8f57d336d16f171549be604526bf98b3f4d01
Cesar Noreña <cnorena@tappsi.co>
2018-02-26 03:44:38 -0300

    Improve splash screen disposables

--- app/src/main/kotlin/co/cesarnorena/pokedex/app/PokemonApplication.kt
+++ app/src/main/kotlin/co/cesarnorena/pokedex/app/PokemonApplication.kt
@@ -1,17 +1,5 @@
 package co.cesarnorena.pokedex.app
 
 import android.app.Application
-import io.realm.Realm
 
-class PokemonApplication : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-        initRealm()
-    }
-
-    private fun initRealm() {
-        Realm.init(this)
-    }
-
-}
+class PokemonApplication : Application()
--- app/src/main/kotlin/co/cesarnorena/pokedex/app/presenter/splash/SplashPresenter.kt
+++ app/src/main/kotlin/co/cesarnorena/pokedex/app/presenter/splash/SplashPresenter.kt
@@ -2,38 +2,47 @@ package co.cesarnorena.pokedex.app.presenter.splash
 
 import co.cesarnorena.pokedex.domain.interactors.CkeckDatabase
 import co.cesarnorena.pokedex.domain.interactors.GetPokedex
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 
 class SplashPresenter(private val view: SplashContract.View,
                       private val checkDatabase: CkeckDatabase,
                       private val getPokedex: GetPokedex) : SplashContract.Presenter {
 
-    private var mDisposable: Disposable? = null
+    private var mDisposable = CompositeDisposable()
 
     override fun onCreateView() {
-        getPokedex()
+        checkDatabse().also {
+            mDisposable.add(it)
+        }
     }
 
     override fun onDestroyView() {
-        mDisposable?.dispose()
+        mDisposable.dispose()
     }
 
-    private fun getPokedex() {
-        mDisposable = checkDatabase.execute().subscribe(
+    private fun checkDatabse(): Disposable {
+        return checkDatabase.execute().subscribe(
                 {
                     view.navigateToPokemonList()
                 },
                 {
-                    val request = GetPokedex.Input(1)
-                    getPokedex.execute(request).subscribe(
-                            { _ ->
-                                view.navigateToPokemonList()
-                            },
-                            { error ->
-                                error.printStackTrace()
-                            })
+                    getPokedex().also {
+                        mDisposable.add(it)
+                    }
                 }
         )
+    }
+
+    private fun getPokedex(): Disposable {
+        val request = GetPokedex.Input(1)
+        return getPokedex.execute(request).subscribe(
+                { _ ->
+                    view.navigateToPokemonList()
+                },
+                { error ->
+                    error.printStackTrace()
+                })
     }
 
 }
